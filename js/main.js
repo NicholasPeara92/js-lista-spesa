229c5e0acd8cfbb2936bc44fe8a7cd878d8c8283
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-27 14:17:10 +0200

    Aggiunge lista della spesa generata

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+"use strict";
+
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, 
 // basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+const shoppingList = ["acqua","pane", "vino", "formaggio", "birra", "pasta", "olio"];
+
+let i = 0;
+
+const listContainer = document.getElementById("list");
+
+while ( i < shoppingList.length ) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = shoppingList[i];
+    listContainer.append(listItem);
+    i++
+}
